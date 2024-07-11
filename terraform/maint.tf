875c4e7c5ab5448b9d6b52d15af3f367ad39506d
Alex <alex15_mdrd_@hotmail.com>
2024-07-11 08:56:34 +0200

    subiendo algun archivo nuevo

---
+++ terraform/maint.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "example" {
+  name                = "exampleacr123"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
